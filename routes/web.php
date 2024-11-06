5624a97cb367dc39a4e461cd9cb52f43b1067f89
Filip Keri <filipkeri@Filips-MacBook-Air.local>
2024-11-06 14:37:18 +0100

    Intro to Routing (3.4)

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/jobs', function () {
+    return '<h1>Available Jobs</h1>';
+})->name('jobs');
+
+Route::get('/test', function () {
+    $url = route('jobs');
+    return "<a href='$url'>
+        Click here
+    </a>";
+});
+
+Route::get('/api/users', function () {
+    return [
+        'name' => 'John Doe',
+        'email' => 'john@gmail.com'
+    ];
+});
