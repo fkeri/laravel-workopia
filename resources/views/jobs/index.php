289bb974d8358be4d51cfef81488f2f976ae576f
Filip Keri <filipkeri@Filips-MacBook-Air.local>
2024-11-06 16:11:17 +0100

    Passing Data to Views (4.3)

--- resources/views/jobs/index.php
+++ resources/views/jobs/index.php
@@ -8,12 +8,11 @@
 </head>
 
 <body>
-    <h1>Available Jobs</h1>
+    <h1><?php echo $title; ?></h1>
     <ul>
-        <li>Web Developer</li>
-        <li>Database Admin</li>
-        <li>Software Engineer</li>
-        <li>Systems Analyst</li>
+        <?php foreach ($jobs as $job) : ?>
+            <li><?php echo htmlspecialchars($job, ENT_QUOTES, 'UTF-8'); ?></li>
+        <?php endforeach; ?>
     </ul>
 </body>
 
--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,17 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function () {
-    return view('jobs.index');
+    $title = 'Available Jobs';
+    $jobs = [
+        'Web Developer',
+        'Database Admin',
+        'Software Engineer',
+        'Systems Analyst'
+    ];
+    // return view('jobs.index', [
+    //     'title' => 'Available Jobs'
+    // ]);
+    return view('jobs.index', compact('title', 'jobs'));
 })->name('jobs');
 
 Route::get('/jobs/create', function () {
