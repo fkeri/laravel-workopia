acb84a9f17df69913d6a3816a16cdd7414277ab0
Filip Keri <filipkeri@Filips-MacBook-Air.local>
2024-11-06 15:39:30 +0100

    Response Helper (3.7)

--- routes/web.php
+++ routes/web.php
@@ -11,18 +11,14 @@ Route::get('/jobs', function () {
     return '<h1>Available Jobs</h1>';
 })->name('jobs');
 
-Route::get('/test', function (Request $request) {
-    return [
-        'method' => $request->method(),
-        'url' => $request->url(),
-        'path' => $request->path(),
-        'fullUrl' => $request->fullUrl(),
-        'ip' => $request->ip(),
-        'userAgent' => $request->userAgent(),
-        'header' => $request->header(),
-    ];
+Route::get('/test', function () {
+    return response('<h1>Hello World</h1>', 200)->header('Content-Type', 'text/html');
 });
 
-Route::get('/users', function (Request $request) {
-    return $request->query('name');
+Route::get('/notfound', function () {
+    return response('Page Not Found', 404);
+});
+
+Route::get('/download', function () {
+    return response()->download(public_path('favicon.ico'));
 });
