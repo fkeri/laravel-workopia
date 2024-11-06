71982d44092614e961ef981a984b14d2abb43f1f
Filip Keri <filipkeri@Filips-MacBook-Air.local>
2024-11-06 15:50:19 +0100

    Create and Display Views (4.2)

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/jobs', function () {
-    return '<h1>Available Jobs</h1>';
+    return view('jobs.index');
 })->name('jobs');
 
-Route::get('/test', function () {
-    return response('<h1>Hello World</h1>', 200)->header('Content-Type', 'text/html');
-});
-
-Route::get('/notfound', function () {
-    return response('Page Not Found', 404);
-});
-
-Route::get('/download', function () {
-    return response()->download(public_path('favicon.ico'));
-});
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
+})->name('jobs');
