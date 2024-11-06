31059fdecb6eebd35e8e241236e23a6ba3a49e6b
Filip Keri <filipkeri@Filips-MacBook-Air.local>
2024-11-06 15:24:18 +0100

    Request Object and Query Params (3.6)

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,10 +11,18 @@ Route::get('/jobs', function () {
     return '<h1>Available Jobs</h1>';
 })->name('jobs');
 
-Route::get('/posts/{id}', function (string $id) {
-    return 'Post ' . $id;
-})->whereNumber('id');
+Route::get('/test', function (Request $request) {
+    return [
+        'method' => $request->method(),
+        'url' => $request->url(),
+        'path' => $request->path(),
+        'fullUrl' => $request->fullUrl(),
+        'ip' => $request->ip(),
+        'userAgent' => $request->userAgent(),
+        'header' => $request->header(),
+    ];
+});
 
-Route::get('/posts/{id}/comments/{commentId}', function (string $id, string $commentId) {
-    return 'Post ' . $id . ' Comment ' . $commentId;
+Route::get('/users', function (Request $request) {
+    return $request->query('name');
 });
