86359f9f23f22de34ab858c89de5ac8d23a62bfa
Filip Keri <filipkeri@Filips-MacBook-Air.local>
2024-11-07 15:35:20 +0100

    Mobile Menu Nav Link (5.9)

--- resources/views/components/button-link.blade.php
+++ resources/views/components/button-link.blade.php
@@ -3,12 +3,13 @@
     'bgClass' => 'bg-yellow-500',
     'hoverClass' => 'hover:bg-yellow-600',
     'icon' => null,
-    'textClass' => 'text-black'
+    'textClass' => 'text-black',
+    'block' => false
 ])
 
 <a
     href="{{$url}}"
-    class="{{$bgClass}} {{$hoverClass}} {{$textClass}} px-4 py-2 rounded hover:shadow-md transition duration-300"
+    class="{{$block ? 'block' : ''}} {{$bgClass}} {{$hoverClass}} {{$textClass}} px-4 py-2 rounded hover:shadow-md transition duration-300"
 >
     @if($icon)
         <i class="fa fa-{{$icon}}"></i>
