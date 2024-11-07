8fad4d9fee81de4abfea2b68c4b24c83527f1370
Filip Keri <filipkeri@Filips-MacBook-Air.local>
2024-11-07 16:08:57 +0100

    Hero Component (5.11)

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,13 +1,13 @@
 <header class="bg-blue-900 text-white p-4">
     <div class="container mx-auto flex justify-between items-center">
         <h1 class="text-3xl font-semibold">
-            <a href="{{url('/')}}">Workopia</a>
+            <a href="{{ url('/') }}">Workopia</a>
         </h1>
         <nav class="hidden md:flex items-center space-x-4">
             <x-nav-link url="/" :active="request()->is('/')">
                 Home
             </x-nav-link>
-            
+
             <x-nav-link url="/jobs" :active="request()->is('jobs')">
                 All Jobs
             </x-nav-link>
@@ -32,18 +32,12 @@
                 Create Job
             </x-button-link>
         </nav>
-        <button
-            id="hamburger"
-            class="text-white md:hidden flex items-center"
-        >
+        <button id="hamburger" class="text-white md:hidden flex items-center">
             <i class="fa fa-bars text-2xl"></i>
         </button>
     </div>
     <!-- Mobile Menu -->
-    <nav
-        id="mobile-menu"
-        class="hidden md:hidden bg-blue-900 text-white mt-5 pb-4 space-y-2"
-    >
+    <nav id="mobile-menu" class="hidden md:hidden bg-blue-900 text-white mt-5 pb-4 space-y-2">
         <x-nav-link url="/jobs" :active="request()->is('jobs')" :mobile="true">
             All Jobs
         </x-nav-link>
