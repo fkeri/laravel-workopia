e630894e5c100ea963fac72241b51cd224c72a8e
Filip Keri <filipkeri@Filips-MacBook-Air.local>
2024-11-26 11:32:40 +0100

    Alpine.js & Alert Dismiss (8.8)

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -10,6 +10,7 @@
         crossorigin="anonymous" referrerpolicy="no-referrer" />
     @vite('resources/css/app.css')
     <link rel="stylesheet" href={{ asset('css/style.css') }}>
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <title>{{ $title ?? 'Workopia | Find and list jobs' }}</title>
 </head>
 
@@ -20,6 +21,13 @@
         <x-top-banner />
     @endif
     <main class="container mx-auto p-4 mt-4">
+        {{-- Display alert messages --}}
+        @if (session('success'))
+            <x-alert type="success" message="{{ session('success') }}" />
+        @endif
+        @if (session('error'))
+            <x-alert type="error" message="{{ session('error') }}" />
+        @endif
         {{ $slot }}
     </main>
 
