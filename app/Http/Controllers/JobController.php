d8ba8705cb285f219fae119b3f23ef90bdb8691d
Filip Keri <filipkeri@Filips-MacBook-Air.local>
2024-11-26 16:22:24 +0100

    Login+Register Controllers & Routes (9.5)

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -10,26 +10,23 @@ use Illuminate\Support\Facades\Storage;
 
 class JobController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    // @desc Show all job listings
+    // @route GET /jobs
     public function index(): View
     {
         $jobs = Job::all();
         return view('jobs.index')->with('jobs', $jobs);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
+    // @desc Show create job form
+    // @route GET /jobs/create
     public function create(): View
     {
         return view('jobs.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+    // @desc Save job listing to DB
+    // @route POST /jobs
     public function store(Request $request): RedirectResponse
     {
         // Convert "true"/"false" to actual boolean values
@@ -76,25 +73,21 @@ class JobController extends Controller
         return redirect()->route('jobs.index')->with('success', 'Job listing created successfully!');
     }
 
-    /**
-     * Display the specified resource.
-     */
+    // @desc Show single job listing
+    // @route GET /jobs/{$id}
     public function show(Job $job): View
     {
         return view('jobs.show')->with('job', $job);
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
+    // @desc Show edit job form
+    // @route GET /jobs/{$id}/edit
     public function edit(Job $job): View
     {
         return view('jobs.edit')->with('job', $job);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+    // @desc Update single job listing
+    // @route PUT /jobs/{$id}
     public function update(Request $request, Job $job): string
     {
         // Convert "true"/"false" to actual boolean values
@@ -141,9 +134,8 @@ class JobController extends Controller
         return redirect()->route('jobs.index')->with('success', 'Job listing updated successfully!');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+    // @desc Delete single job listing
+    // @route DELETE /jobs/{$id}
     public function destroy(Job $job): RedirectResponse
     {
         // If logo, then delete it
