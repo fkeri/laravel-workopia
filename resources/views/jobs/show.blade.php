7ae0d28495c3660ca1f7a424bd9e3927ba8ac5d3
Filip Keri <filipkeri@Filips-MacBook-Air.local>
2024-11-26 12:21:59 +0100

    Uploading images to local storage (8.10)

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -86,7 +86,7 @@
                 Company Info
             </h3>
             @if ($job->company_logo)
-                <img src="/images/{{ $job->company_logo }}" alt="{{ $job->company_name }}"
+                <img src="/storage/{{ $job->company_logo }}" alt="{{ $job->company_name }}"
                     class="w-full rounded-lg mb-4 m-auto" />
             @endif
             <h4 class="text-lg font-bold">{{ $job->company_name }}</h4>
