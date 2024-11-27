7094cd849a6e6e40a10e1ed7208b8abd34f5b47f
Filip Keri <filipkeri@Filips-MacBook-Air.local>
2024-11-27 17:06:10 +0100

    Bookmarking Jobs (12.5)

--- app/Http/Controllers/BookmarkController.php
+++ app/Http/Controllers/BookmarkController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Job;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
@@ -13,8 +15,24 @@ class BookmarkController extends Controller
     public function index(): View {
         $user = Auth::user();
 
-        $bookmarks = $user->bookmarkedJobs()->paginate(9);
+        $bookmarks = $user->bookmarkedJobs()->orderBy('job_user_bookmarks.created_at', 'desc')->paginate(9);
 
         return view('jobs.bookmarked')->with('bookmarks', $bookmarks);
+    }
+
+    // @desc    Create new bookmark
+    // @route   POST /bookmarks/{$job}
+    public function store(Job $job): RedirectResponse {
+        $user = Auth::user();
+
+        // Check if the job is already bookmarked
+        if ($user->bookmarkedJobs()->where('job_id', $job->id)->exists()) {
+            return back()->with('status', 'Job is already bookmarked!');
+        }
+
+        // Create new bookmark
+        $user->bookmarkedJobs()->attach($job->id);
+
+        return back()->with('success', 'Job bookmarked successfully!');
     }
 }
