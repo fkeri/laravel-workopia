8c3d39d81a8d36ef5de85fea694a8cbf1b284c72
Filip Keri <filipkeri@Filips-MacBook-Air.local>
2024-11-27 14:30:35 +0100

    Add Dashboard Controller & View (11.2)

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Job;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
+
+class DashboardController extends Controller
+{
+    // @desc    Show all user's job listings
+    // @route   GET /dashboard
+    public function index(): View {
+        // Get logged in user
+        $user = Auth::user();
+
+        // Get the user's listings
+        $jobs = Job::where('user_id', $user->id)->get();
+
+        return view('dashboard.index', compact('user', 'jobs'));
+    }
+}
