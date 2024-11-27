f8013cf327f5f9154940b11a01984bc879b571a3
Filip Keri <filipkeri@Filips-MacBook-Air.local>
2024-11-27 12:56:56 +0100

    Protecting Routes - Auth Middleware (10.3)

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,13 @@ use App\Http\Controllers\RegisterController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::resource('jobs', JobController::class);
+// Route::resource('jobs', JobController::class);
+Route::resource('jobs', JobController::class)->middleware('auth')->only([
+  'create', 'edit', 'update', 'destroy'
+]);
+Route::resource('jobs', JobController::class)->except([
+  'create', 'edit', 'update', 'destroy'
+]);
 
 // Registration routes
 Route::get('/register', [RegisterController::class, 'register'])->name('register');
